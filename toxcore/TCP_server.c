84e07a718bc36c929ebfcba085bdc6f214e3babd
Ansa89 <ansalonistefano@gmail.com>
2016-10-25 15:43:48 +0200

    TCP_server.c: fix windows headers

--- toxcore/TCP_server.c
+++ toxcore/TCP_server.c
@@ -30,9 +30,11 @@
 
 #if !defined(_WIN32) && !defined(__WIN32__) && !defined (WIN32)
 #include <sys/ioctl.h>
+#include <arpa/inet.h>
+#else
+#include <winsock2.h>
 #endif
 
-#include <arpa/inet.h>
 #include "util.h"
 #include "nat_traversal.h"
 
