ee59fd8aeda7294783aa8d4573ea4a1df30bed47
Ansa89 <ansalonistefano@gmail.com>
2016-11-15 16:17:16 +0100

    NAT traversal: add IPv6 option

--- toxcore/TCP_server.c
+++ toxcore/TCP_server.c
@@ -1098,7 +1098,11 @@ TCP_Server *new_TCP_server_nat(Logger *log, uint8_t ipv6_enabled, uint16_t num_s
 
 #endif
 
-            nat_map_port(log, traversal_type, NAT_TRAVERSAL_TCP, ports[i], NULL);
+            if (ipv6_enabled) {
+                nat_map_port(log, traversal_type, NAT_TRAVERSAL_TCP, ports[i], true, NULL);
+            }
+
+            nat_map_port(log, traversal_type, NAT_TRAVERSAL_TCP, ports[i], false, NULL);
 
             temp->socks_listening[temp->num_listening_socks] = sock;
             ++temp->num_listening_socks;
