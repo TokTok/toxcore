1e61fd3c28b3d826338ff5a56e3acb73e9a18006
pranomostro <pranomestro@gmail.com>
2016-12-03 16:06:07 +0100

    Convert file_saving_test to standard C, add it to the auto_tests.

--- auto_tests/file_saving_test.c
+++ auto_tests/file_saving_test.c
@@ -3,7 +3,7 @@
  * Small test for checking if obtaining savedata, saving it to disk and using
  * works correctly.
  *
- *  Copyright (C) 2013 Tox project All Rights Reserved.
+ *  Copyright (C) 2016 Tox project All Rights Reserved.
  *
  *  This file is part of Tox.
  *
@@ -22,112 +22,106 @@
  *
  */
 
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include <tox/tox.h>
 #include <tox/toxencryptsave.h>
 
-char* pphrase="bar", * name="foo";
+static const char *pphrase = "bar", * name = "foo";
 
 void tse(void)
 {
-	Tox* t;
-	struct Tox_Options to;
-	uint8_t* clear, * cipher;
-	off_t sz;
-	int fd;
-	TOX_ERR_ENCRYPTION eerr;
+    Tox *t;
+    struct Tox_Options to;
+    uint8_t *clear, * cipher;
+    off_t sz;
+    FILE *f;
+    TOX_ERR_ENCRYPTION eerr;
 
-	tox_options_default(&to);
-	t=tox_new(&to, NULL);
+    tox_options_default(&to);
+    t = tox_new(&to, NULL);
 
-	tox_self_set_name(t, (uint8_t*)name, strlen((char*)name), NULL);
+    tox_self_set_name(t, (uint8_t *)name, strlen((char *)name), NULL);
 
-	fd = open("save", O_WRONLY | O_TRUNC | O_CREAT , 0666);
+    f = fopen("save", "w");
 
-	sz = tox_get_savedata_size(t);
-	clear = malloc(sz);
+    sz = tox_get_savedata_size(t);
+    clear = malloc(sz);
 
-	/*this function does not write any data at all*/
-	tox_get_savedata(t, clear);
+    /*this function does not write any data at all*/
+    tox_get_savedata(t, clear);
 
-	sz += TOX_PASS_ENCRYPTION_EXTRA_LENGTH;
-	cipher = malloc(sz);
-	if(!tox_pass_encrypt(clear, sz - TOX_PASS_ENCRYPTION_EXTRA_LENGTH, (uint8_t*)pphrase, strlen(pphrase), cipher, &eerr))
-	{
-		fprintf(stderr, "error: could not encrypt, error code %d\n", eerr);
-		exit(4);
-	}
+    sz += TOX_PASS_ENCRYPTION_EXTRA_LENGTH;
+    cipher = malloc(sz);
 
-	write(fd, cipher, sz);
-	fsync(fd);
+    if (!tox_pass_encrypt(clear, sz - TOX_PASS_ENCRYPTION_EXTRA_LENGTH, (uint8_t *)pphrase, strlen(pphrase), cipher,
+                          &eerr)) {
+        fprintf(stderr, "error: could not encrypt, error code %d\n", eerr);
+        exit(4);
+    }
 
-	free(cipher);
-	free(clear);
-	close(fd);
-	tox_kill(t);
+    fwrite(cipher, sz, sizeof(*cipher), f);
+
+    free(cipher);
+    free(clear);
+    fclose(f);
+    tox_kill(t);
 }
 
 void tsd(void)
 {
-	uint8_t readname[TOX_MAX_NAME_LENGTH];
-	uint8_t* clear, * cipher;
-	off_t sz;
-	int fd;
-	Tox* t;
-	struct Tox_Options to;
-	TOX_ERR_NEW err;
-	TOX_ERR_DECRYPTION derr;
+    uint8_t readname[TOX_MAX_NAME_LENGTH];
+    uint8_t *clear, * cipher;
+    int sz;
+    FILE *f;
+    Tox *t;
+    struct Tox_Options to;
+    TOX_ERR_NEW err;
+    TOX_ERR_DECRYPTION derr;
 
-	fd = open("save", O_RDONLY , 0666);
-	sz = lseek(fd, 0, SEEK_END);
-	lseek(fd, 0, SEEK_SET);
+    f = fopen("save", "r");
+    sz = fseek(f, 0, SEEK_END);
+    fseek(f, 0, SEEK_SET);
 
-	cipher = malloc(sz);
-	clear = malloc(sz - TOX_PASS_ENCRYPTION_EXTRA_LENGTH);
-	read(fd, cipher, sz);
+    cipher = malloc(sz);
+    clear = malloc(sz - TOX_PASS_ENCRYPTION_EXTRA_LENGTH);
+    fgets((char *)cipher, sz, f);
 
-	if(!tox_pass_decrypt(cipher, sz, (uint8_t*)pphrase, strlen(pphrase), clear, &derr))
-	{
-		fprintf(stderr, "error: could not decrypt, error code %d\n", derr);
-		exit(3);
-	}
+    if (!tox_pass_decrypt(cipher, sz, (uint8_t *)pphrase, strlen(pphrase), clear, &derr)) {
+        fprintf(stderr, "error: could not decrypt, error code %d\n", derr);
+        exit(3);
+    }
 
-	tox_options_default(&to);
-	to.savedata_type = TOX_SAVEDATA_TYPE_TOX_SAVE;
-	to.savedata_data = clear;
-	if((t = tox_new(&to, &err)) == NULL)
-	{
-		fprintf(stderr, "error: tox_new returned the error value %d\n", err);
-		exit(1);
-	}
+    tox_options_default(&to);
+    to.savedata_type = TOX_SAVEDATA_TYPE_TOX_SAVE;
+    to.savedata_data = clear;
 
-	tox_self_get_name(t, readname);
-	name[tox_self_get_name_size(t)] = '\0';
+    if ((t = tox_new(&to, &err)) == NULL) {
+        fprintf(stderr, "error: tox_new returned the error value %d\n", err);
+        exit(1);
+    }
 
-	if(strcmp((char*)readname, name))
-	{
-		fprintf(stderr, "error: name returned by tox_self_get_name does not match expected result\n");
-		exit(2);
-	}
+    tox_self_get_name(t, readname);
+    readname[tox_self_get_name_size(t)] = '\0';
 
-	free(cipher);
-	free(clear);
-	close(fd);
-	tox_kill(t);
+    if (strcmp((char *)readname, name)) {
+        fprintf(stderr, "error: name returned by tox_self_get_name does not match expected result\n");
+        exit(2);
+    }
+
+    free(cipher);
+    free(clear);
+    fclose(f);
+    tox_kill(t);
 }
 
 int main(void)
 {
-	tse();
-	tsd();
+    tse();
+    tsd();
 
-	return 0;
+    return 0;
 }
 
