72ccc1a19ff989a7578f2360c0f08a910e73ea43
Diadlo <polsha3@gmail.com>
2017-01-04 01:26:35 +0300

    Remove unused includes from network.h

--- toxcore/network.h
+++ toxcore/network.h
@@ -54,11 +54,7 @@ typedef short sa_family_t;
 
 #include <arpa/inet.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <netdb.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #endif
