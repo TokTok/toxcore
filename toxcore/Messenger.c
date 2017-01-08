a9e77c0b228143582435fe87944b84a2af433759
Ansa89 <ansalonistefano@gmail.com>
2017-01-08 19:24:26 +0100

    Tox loop: cosmetic fixes

--- toxcore/Messenger.c
+++ toxcore/Messenger.c
@@ -2079,7 +2079,7 @@ void kill_messenger(Messenger *m)
         clear_receipts(m, i);
     }
 
-#ifdef  HAVE_LIBEV
+#ifdef HAVE_LIBEV
     ev_loop_destroy(m->dispatcher);
 #elif HAVE_LIBEVENT
     event_base_free(m->dispatcher);
