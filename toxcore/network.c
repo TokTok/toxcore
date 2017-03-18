7fa5e2b7671eef66aaa6825d66621edb6ce01d38
Diadlo <polsha3@gmail.com>
2017-03-18 22:22:28 +0300

    Add loopback and broadcast constants

--- toxcore/network.c
+++ toxcore/network.c
@@ -138,6 +138,18 @@ static int inet_pton(Family family, const char *addrString, void *addrbuf)
 
 #endif
 
+const IP4 IP4_LOOPBACK = { INADDR_LOOPBACK };
+const IP6 IP6_LOOPBACK = {
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }
+};
+
+
+const IP4 IP4_BROADCAST = { INADDR_BROADCAST };
+const IP6 IP6_BROADCAST = {
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff }
+};
+
+
 static int make_proto(int proto);
 static int make_socktype(int type);
 static int make_family(int tox_family);
--- toxcore/network.h
+++ toxcore/network.h
@@ -130,11 +130,14 @@ typedef enum NET_PACKET_TYPE {
 #define TCP_FAMILY (TOX_AF_INET6 + 4)
 
 typedef union {
-    uint8_t uint8[4];
-    uint16_t uint16[2];
     uint32_t uint32;
+    uint16_t uint16[2];
+    uint8_t uint8[4];
 }
 IP4;
+
+extern const IP4 IP4_LOOPBACK;
+extern const IP4 IP4_BROADCAST;
 
 typedef union {
     uint8_t uint8[16];
@@ -143,6 +146,9 @@ typedef union {
     uint64_t uint64[2];
 }
 IP6;
+
+extern const IP6 IP6_LOOPBACK;
+extern const IP6 IP6_BROADCAST;
 
 typedef struct {
     uint8_t family;
