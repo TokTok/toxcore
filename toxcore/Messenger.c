6a16a4d1b9368851ca98b534f4d158c9aab451e8
Diadlo <polsha3@gmail.com>
2018-01-15 01:42:30 +0300

    Add correction message type

--- toxcore/Messenger.c
+++ toxcore/Messenger.c
@@ -486,7 +486,7 @@ int m_friend_exists(const Messenger *m, int32_t friendnumber)
 int m_send_message_generic(Messenger *m, int32_t friendnumber, uint8_t type, const uint8_t *message, uint32_t length,
                            uint32_t *message_id)
 {
-    if (type > MESSAGE_ACTION) {
+    if (type > MESSAGE_CORRECTION) {
         return -5;
     }
 
@@ -2199,7 +2199,8 @@ static int m_handle_packet(void *object, int i, const uint8_t *temp, uint16_t le
         }
 
         case PACKET_ID_MESSAGE: // fall-through
-        case PACKET_ID_ACTION: {
+        case PACKET_ID_ACTION:
+        case PACKET_ID_CORRECTION: {
             if (data_length == 0) {
                 break;
             }
