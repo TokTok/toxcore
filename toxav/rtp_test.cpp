8d26c70eb53abb87088835e198a910739a4db6f8
iphydf <iphydf@users.noreply.github.com>
2018-01-21 22:38:08 +0000

    Manually serialise RTPHeader struct instead of memcpy.

---
+++ toxav/rtp_test.cpp
@@ -0,0 +1,53 @@
+#include "rtp.h"
+
+#include "../toxcore/crypto_core.h"
+
+#include <byteswap.h>
+#include <gtest/gtest.h>
+
+// Reference implementation.
+static void rtp_header_pack_ref(uint8_t *rdata, struct RTPHeader const *header)
+{
+    struct RTPHeader hton_header = *header;
+    hton_header.sequnum = net_htons(hton_header.sequnum);
+    hton_header.timestamp = net_htonl(hton_header.timestamp);
+    hton_header.ssrc = net_htonl(hton_header.ssrc);
+    hton_header.flags = bswap_64(hton_header.flags);
+    hton_header.offset_full = net_htonl(hton_header.offset_full);
+    hton_header.data_length_full = net_htonl(hton_header.data_length_full);
+    hton_header.received_length_full = net_htonl(hton_header.received_length_full);
+    for (size_t i = 0; i < sizeof hton_header.csrc / sizeof hton_header.csrc[0]; i++) {
+        hton_header.csrc[i] = net_htonl(hton_header.csrc[i]);
+    }
+    hton_header.offset_lower = net_htons(hton_header.offset_lower);
+    hton_header.data_length_lower = net_htons(hton_header.data_length_lower);
+    memcpy(rdata, &hton_header, sizeof(struct RTPHeader));
+}
+
+TEST(Rtp, Serialisation) {
+    RTPHeader header;
+    random_bytes((uint8_t *)&header, sizeof header);
+
+    uint8_t rdata[sizeof(RTPHeader)];
+    rtp_header_pack(rdata, &header);
+
+    uint8_t rdata_ref[sizeof(RTPHeader)];
+    rtp_header_pack_ref(rdata_ref, &header);
+
+    EXPECT_EQ(std::string((char const *)rdata, sizeof rdata),
+              std::string((char const *)rdata_ref, sizeof rdata_ref));
+}
+
+TEST(Rtp, Deserialisation) {
+    RTPHeader header;
+    random_bytes((uint8_t *)&header, sizeof header);
+
+    uint8_t rdata[sizeof(RTPHeader)];
+    EXPECT_EQ(rtp_header_pack(rdata, &header), RTP_HEADER_SIZE);
+
+    RTPHeader unpacked;
+    EXPECT_EQ(rtp_header_unpack(rdata, &unpacked), RTP_HEADER_SIZE);
+
+    EXPECT_EQ(std::string((char const *)&header, sizeof header),
+              std::string((char const *)&unpacked, sizeof unpacked));
+}
