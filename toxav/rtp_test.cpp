99bbc36b6c6ad2286ccb1828481f52eb2fd6a332
iphydf <iphydf@users.noreply.github.com>
2018-02-02 15:37:19 +0000

    Remove csrc from the RTPHeader struct.
    This is not used by anything in the code, so we shouldn't have it in the
    header.

--- toxav/rtp_test.cpp
+++ toxav/rtp_test.cpp
@@ -4,16 +4,53 @@
 
 #include <gtest/gtest.h>
 
-TEST(Rtp, Deserialisation) {
-    RTPHeader header;
-    random_bytes((uint8_t *)&header, sizeof header);
+namespace
+{
+RTPHeader random_header()
+{
+    return {
+        random_u16(),
+        random_u16(),
+        random_u16(),
+        random_u16(),
+        random_u16(),
+        random_u16(),
+        random_u16(),
+        random_u32(),
+        random_u32(),
+        random_u64(),
+        random_u32(),
+        random_u32(),
+        random_u32(),
+        random_u16(),
+        random_u16(),
+    };
+}
+}  // namespace
+
+TEST(Rtp, Deserialisation)
+{
+    RTPHeader const header = random_header();
 
     uint8_t rdata[sizeof(RTPHeader)];
     EXPECT_EQ(rtp_header_pack(rdata, &header), RTP_HEADER_SIZE);
 
-    RTPHeader unpacked;
+    RTPHeader unpacked = {0};
     EXPECT_EQ(rtp_header_unpack(rdata, &unpacked), RTP_HEADER_SIZE);
 
-    EXPECT_EQ(std::string((char const *)&header, sizeof header),
-              std::string((char const *)&unpacked, sizeof unpacked));
+    EXPECT_EQ(header.protocol_version, unpacked.protocol_version);
+    EXPECT_EQ(header.pe, unpacked.pe);
+    EXPECT_EQ(header.xe, unpacked.xe);
+    EXPECT_EQ(header.cc, unpacked.cc);
+    EXPECT_EQ(header.ma, unpacked.ma);
+    EXPECT_EQ(header.pt, unpacked.pt);
+    EXPECT_EQ(header.sequnum, unpacked.sequnum);
+    EXPECT_EQ(header.timestamp, unpacked.timestamp);
+    EXPECT_EQ(header.ssrc, unpacked.ssrc);
+    EXPECT_EQ(header.flags, unpacked.flags);
+    EXPECT_EQ(header.offset_full, unpacked.offset_full);
+    EXPECT_EQ(header.data_length_full, unpacked.data_length_full);
+    EXPECT_EQ(header.received_length_full, unpacked.received_length_full);
+    EXPECT_EQ(header.offset_lower, unpacked.offset_lower);
+    EXPECT_EQ(header.data_length_lower, unpacked.data_length_lower);
 }
