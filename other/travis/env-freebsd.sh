6b4ed86a1ffc80bb1910a5528119643d9baf9fc5
iphydf <iphydf@users.noreply.github.com>
2018-02-25 10:00:56 +0000

    Fix warning on Mac OS X and FreeBSD.
    This currently fails the nightly build.

--- other/travis/env-freebsd.sh
+++ other/travis/env-freebsd.sh
@@ -17,7 +17,7 @@ SCREEN_SESSION=freebsd
 SSH_PORT=10022
 
 RUN() {
-  ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@localhost -p $SSH_PORT "$@"
+  ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@localhost -p $SSH_PORT \""$@"\"
 }
 
 TESTS() {
