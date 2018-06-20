72ad2acade926286848aec7d36ee4351a0491cc8
hugbubby <hugbubby@protonmail.com>
2018-06-20 10:58:59 -0700

    Grrrrrr

--- auto_tests/TCP_test.c
+++ auto_tests/TCP_test.c
@@ -58,10 +58,10 @@ START_TEST(test_basic)
     ck_assert_msg(tcp_server_listen_count(tcp_s) == NUM_PORTS, "Failed to bind a TCP relay server to all %d attempted ports", NUM_PORTS);
 
     //Check all opened ports for connectivity.
-    Socket sock = net_socket(net_family_ipv6, TOX_SOCK_STREAM, TOX_PROTO_TCP);
-    IP_Port ip_port_loopback;
-    ip_port_loopback.ip = get_loopback();
     for (uint8_t i = 0; i < NUM_PORTS; i++) {
+        Socket sock = net_socket(net_family_ipv6, TOX_SOCK_STREAM, TOX_PROTO_TCP);
+        IP_Port ip_port_loopback;
+        ip_port_loopback.ip = get_loopback();
         ip_port_loopback.port = net_htons(ports[i]);
         int ret = net_connect(sock, ip_port_loopback);
         ck_assert_msg(ret == 0, "Failed to connect to created TCP relay server on port %d", ports[i]);
