fb9353523c7943b20d04413411fbe55a58ba1fad
hugbubby <hugbubby@protonmail.com>
2018-07-06 02:25:49 -0700

    back to old way, save comments and one change

--- auto_tests/version_test.c
+++ auto_tests/version_test.c
@@ -1,44 +1,39 @@
 #include "../toxcore/tox.h"
-
 #include "check_compat.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
-#define check_good_if(argument)                            \
-    do_check(TOX_VERSION_MAJOR, TOX_VERSION_MINOR, TOX_VERSION_PATCH,     \
+#define check(major, minor, patch, expected)                            \
+  do_check(TOX_VERSION_MAJOR, TOX_VERSION_MINOR, TOX_VERSION_PATCH,     \
            major, minor, patch,                                         \
-           TOX_VERSION_IS_API_COMPATIBLE(major, minor, patch), argument)
-
-#define FUZZ_VERSION \
-    for(int major=0; major < 10; major++) { \
-        for(int minor=0; minor < 10; minor++) { \
-            for(int patch = 0; patch < 10; patch++) {
-#define END_FUZZ }}}
+           TOX_VERSION_IS_API_COMPATIBLE(major, minor, patch), expected)
 
 static void do_check(int lib_major, int lib_minor, int lib_patch,
                      int cli_major, int cli_minor, int cli_patch,
                      bool actual, bool expected)
 {
     ck_assert_msg(actual == expected,
-                  "Client version %d.%d.%d is %s compatible with library version %d.%d.%d, but it should %s be.\n",
-                  cli_major, cli_minor, cli_patch, actual ? "" : "not",
-                  lib_major, lib_minor, lib_patch, expected ? "" : "not");
+        "Client version %d.%d.%d is%s compatible with library version %d.%d.%d, but it should%s be\n",
+        cli_major, cli_minor, cli_patch, actual ? "" : " not",
+        lib_major, lib_minor, lib_patch, expected ? "" : " not");
 }
 
 #undef TOX_VERSION_MAJOR
 #undef TOX_VERSION_MINOR
 #undef TOX_VERSION_PATCH
 
-
 int main(void)
 {
 #define TOX_VERSION_MAJOR 0
 #define TOX_VERSION_MINOR 0
 #define TOX_VERSION_PATCH 4
-    FUZZ_VERSION
-    check_good_if(major == 0 && minor == 0 && patch == 4);
-    END_FUZZ
+    // Tox versions from 0.0.* are only compatible with themselves.
+    check(0, 0, 0, false);
+    check(0, 0, 3, false);
+    check(0, 0, 4, true);
+    check(0, 0, 5, false);
+    check(1, 0, 4, false);
 #undef TOX_VERSION_MAJOR
 #undef TOX_VERSION_MINOR
 #undef TOX_VERSION_PATCH
@@ -46,9 +41,20 @@ int main(void)
 #define TOX_VERSION_MAJOR 0
 #define TOX_VERSION_MINOR 1
 #define TOX_VERSION_PATCH 4
-    FUZZ_VERSION
-    check_good_if(major == 0 && minor == 1 && patch <= 4);
-    END_FUZZ
+    // Tox versions from 0.1.* are only compatible with themselves or 0.1.<*
+    check(0, 0, 0, false);
+    check(0, 0, 4, false);
+    check(0, 0, 5, false);
+    check(0, 1, 0, true);
+    check(0, 1, 4, true);
+    check(0, 1, 5, false);
+    check(0, 2, 0, false);
+    check(0, 2, 4, false);
+    check(0, 2, 5, false);
+    check(1, 0, 0, false);
+    check(1, 0, 4, false);
+    check(1, 0, 5, false);
+    check(1, 1, 4, false);
 #undef TOX_VERSION_MAJOR
 #undef TOX_VERSION_MINOR
 #undef TOX_VERSION_PATCH
@@ -56,9 +62,15 @@ int main(void)
 #define TOX_VERSION_MAJOR 1
 #define TOX_VERSION_MINOR 0
 #define TOX_VERSION_PATCH 4
-    FUZZ_VERSION
-    check_good_if(major == 1 && (minor == 0 && patch <= 4));
-    END_FUZZ
+    //Beyond 0.*.* Tox is comfortable with any lower version within their major
+    check(0, 0, 4, false);
+    check(1, 0, 0, true);
+    check(1, 0, 1, true);
+    check(1, 0, 4, true);
+    check(1, 0, 5, false);
+    check(1, 1, 0, false);
+    check(2, 0, 0, false);
+    check(2, 0, 4, false);
 #undef TOX_VERSION_MAJOR
 #undef TOX_VERSION_MINOR
 #undef TOX_VERSION_PATCH
@@ -66,9 +78,19 @@ int main(void)
 #define TOX_VERSION_MAJOR 1
 #define TOX_VERSION_MINOR 1
 #define TOX_VERSION_PATCH 4
-    FUZZ_VERSION
-    check_good_if(major == 1 && (minor < 1 || (minor == 1 && patch <= 4)));
-    END_FUZZ
+    check(0, 0, 4, false);
+    check(1, 0, 0, true);
+    check(1, 0, 4, true);
+    check(1, 0, 5, true);
+    check(1, 1, 0, true);
+    check(1, 1, 1, true);
+    check(1, 1, 4, true);
+    check(1, 1, 5, false);
+    check(1, 2, 0, false);
+    check(1, 2, 4, false);
+    check(1, 2, 5, false);
+    check(2, 0, 0, false);
+    check(2, 1, 4, false);
 #undef TOX_VERSION_MAJOR
 #undef TOX_VERSION_MINOR
 #undef TOX_VERSION_PATCH
