zugz/c-toxcore

less verbose logging

toxcore/logger.h
@@ -27,7 +27,7 @@
 #include <stdint.h>
 
 #ifndef MIN_LOGGER_LEVEL
-#define MIN_LOGGER_LEVEL LOG_DEBUG
+#define MIN_LOGGER_LEVEL LOG_INFO
 #endif
 
 typedef enum {

TokTok/toxcore

toxcore/logger.h
@@ -29,7 +29,7 @@
 #include "ccompat.h"
 
 #ifndef MIN_LOGGER_LEVEL
-#define MIN_LOGGER_LEVEL LOGGER_LEVEL_DEBUG
+#define MIN_LOGGER_LEVEL LOGGER_LEVEL_INFO
 #endif
 
 typedef enum Logger_Level {