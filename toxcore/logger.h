zoff99/c-toxcore

timestamp too old tweak

toxcore/logger.h
@@ -27,7 +27,7 @@
 #include <stdint.h>
 
 #ifndef MIN_LOGGER_LEVEL
-#define MIN_LOGGER_LEVEL LOG_INFO
+#define MIN_LOGGER_LEVEL LOG_DEBUG
 #endif
 
 typedef enum {

TokTok/toxcore

toxcore/logger.h
@@ -29,7 +29,7 @@
 #include "ccompat.h"
 
 #ifndef MIN_LOGGER_LEVEL
-#define MIN_LOGGER_LEVEL LOGGER_LEVEL_INFO
+#define MIN_LOGGER_LEVEL LOGGER_LEVEL_DEBUG
 #endif
 
 typedef enum Logger_Level {