adc64e7e4223b45e41353801389ec3be17394ba0
Zoff <zoff99@users.noreply.github.com>
2018-11-04 13:19:20 +0100

    change comment

--- toxcore/Messenger.h
+++ toxcore/Messenger.h
@@ -87,7 +87,7 @@ typedef struct Messenger_Options {
     uint8_t state_plugins_length;
 } Messenger_Options;
 
-/* this means no special capabilities, toxcore_capabilities == 0 */
+/* this means no special capabilities, Friend.toxcore_capabilities == 0 */
 #define TOX_CAPABILITY_BASIC 0
 
 /* uint64_t ToxCapabilitiesFlags */
