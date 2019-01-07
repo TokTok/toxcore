02636ca0419ea68db5fbf698a410e22969d79e94
iphydf <iphydf@users.noreply.github.com>
2019-01-07 20:13:54 +0000

    Add API for bump-allocator with trivial implementation.
    The implementation is not actually a bump allocator at this time, but
    implements the API correctly. In the future, we can use this to implement
    more efficient local allocations to replace the use of VLAs.

---
+++ toxcore/mem.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2019 The TokTok team.
+ *
+ * This file is part of Tox, the free peer to peer instant messenger.
+ *
+ * Tox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "mem.h"
+
+#include <stdlib.h>
+
+void *mem_bcalloc(uint32_t nmemb, uint32_t size)
+{
+    return calloc(nmemb, size);
+}
+
+void mem_bfree(void *ptr)
+{
+    free(ptr);
+}
