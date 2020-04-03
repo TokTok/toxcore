e58c7aae06e69e28ef35750e892259686d73614d
David Zero <zero-one@zer0-one.net>
2020-04-03 04:21:04 -0700

    Fix TOX_GROUP_MAX_PEER_LENGTH

--- toxcore/tox.api.h
+++ toxcore/tox.api.h
@@ -2852,6 +2852,8 @@ namespace group {
    * Size of a peer public key.
    */
   const PEER_PUBLIC_KEY_SIZE      = 32;
+
+  const MAX_PEER_LENGTH           = 128;
 }
 
 /*******************************************************************************
