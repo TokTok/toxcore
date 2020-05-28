93d24211b67b4b87e5f625748f1836656ddae85d
jfreegman <jfreegman@gmail.com>
2020-05-28 14:23:17 -0400

    Add backwards compatibility code for onion

--- toxcore/onion_announce.h
+++ toxcore/onion_announce.h
@@ -25,6 +25,9 @@
 #define ONION_ANNOUNCE_RESPONSE_MIN_SIZE (2 + ONION_ANNOUNCE_SENDBACK_DATA_LENGTH + CRYPTO_NONCE_SIZE + ONION_PING_ID_SIZE + CRYPTO_MAC_SIZE)
 #define ONION_ANNOUNCE_RESPONSE_MAX_SIZE (ONION_ANNOUNCE_RESPONSE_MIN_SIZE +  GCA_ANNOUNCE_MAX_SIZE * MAX_SENT_NODES)
 
+/* TODO: DEPRECATE */
+#define ONION_ANNOUNCE_REQUEST_SIZE (1 + CRYPTO_NONCE_SIZE + CRYPTO_PUBLIC_KEY_SIZE + ONION_PING_ID_SIZE + CRYPTO_PUBLIC_KEY_SIZE + CRYPTO_PUBLIC_KEY_SIZE + ONION_ANNOUNCE_SENDBACK_DATA_LENGTH + CRYPTO_MAC_SIZE)
+
 #define ONION_DATA_RESPONSE_MIN_SIZE (1 + CRYPTO_NONCE_SIZE + CRYPTO_PUBLIC_KEY_SIZE + CRYPTO_MAC_SIZE)
 
 #if ONION_PING_ID_SIZE != CRYPTO_PUBLIC_KEY_SIZE
@@ -57,6 +60,10 @@ int create_announce_request(uint8_t *packet, uint16_t max_packet_length, const u
                             const uint8_t *public_key, const uint8_t *secret_key, const uint8_t *ping_id, const uint8_t *client_id,
                             const uint8_t *data_public_key, uint64_t sendback_data);
 
+/* TODO: DEPRECATE */
+int create_announce_request_old(uint8_t *packet, uint16_t max_packet_length, const uint8_t *dest_client_id,
+                            const uint8_t *public_key, const uint8_t *secret_key, const uint8_t *ping_id, const uint8_t *client_id,
+                            const uint8_t *data_public_key, uint64_t sendback_data);
 
 int create_gca_announce_request(uint8_t *packet, uint16_t max_packet_length, const uint8_t *dest_client_id,
                                 const uint8_t *public_key, const uint8_t *secret_key, const uint8_t *ping_id,
