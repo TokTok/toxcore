a2845d625af195ae56f305c8886d62c54e2b4456
zgfzgf <1901989065@qq.com>
2020-06-17 16:32:21 +0800

    perf: Optimize code

--- toxcore/DHT.c
+++ toxcore/DHT.c
@@ -753,29 +753,25 @@ static int client_or_ip_port_in_list(const Logger *log, const Mono_Time *mono_ti
 bool add_to_list(Node_format *nodes_list, uint32_t length, const uint8_t *pk, IP_Port ip_port,
                  const uint8_t *cmp_pk)
 {
-    bool ret = false;
-    uint8_t pk_new[CRYPTO_PUBLIC_KEY_SIZE];
-    memcpy(pk_new, pk, CRYPTO_PUBLIC_KEY_SIZE);
-    IP_Port ip_port_new = ip_port;
-
+    uint32_t index = length;
     uint8_t pk_bak[CRYPTO_PUBLIC_KEY_SIZE];
-    IP_Port ip_port_bak;
+    memcpy(pk_bak, pk, CRYPTO_PUBLIC_KEY_SIZE);
 
     for (uint32_t i = 0; i < length; ++i) {
-        if (id_closest(cmp_pk, nodes_list[i].public_key, pk_new) == 2) {
+        if (id_closest(cmp_pk, nodes_list[i].public_key, pk_bak) == 2) {
+            index = i;
             memcpy(pk_bak, nodes_list[i].public_key, CRYPTO_PUBLIC_KEY_SIZE);
-            memcpy(nodes_list[i].public_key, pk_new, CRYPTO_PUBLIC_KEY_SIZE);
-            memcpy(pk_new, pk_bak, CRYPTO_PUBLIC_KEY_SIZE);
-
-            ip_port_bak = nodes_list[i].ip_port;
-            nodes_list[i].ip_port = ip_port_new;
-            ip_port_new = ip_port_bak;
-
-            ret = true;
         }
     }
 
-    return ret;
+    if (index == length) {
+        return false;
+    }
+
+    memcpy(nodes_list[index].public_key, pk, CRYPTO_PUBLIC_KEY_SIZE);
+    nodes_list[index].ip_port = ip_port;
+
+    return true;
 }
 
 /* TODO(irungentoo): change this to 7 when done*/
